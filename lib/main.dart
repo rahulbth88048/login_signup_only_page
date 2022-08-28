5978f39ce5772a165ff259d018d346b2311d6194
Rahul Kumar <105053974+rahulbth88048@users.noreply.github.com>
2022-08-28 15:04:59 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'MyLogin.dart';
+import 'MyRegister.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
